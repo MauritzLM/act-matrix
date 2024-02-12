32fe8b902b346e9564d46503cfb06eaa7289122f
Mauritz Marais <mauritz303@gmail.com>
2024-02-12 13:01:18 +0200

    added currentpage state and conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,49 @@
-import { Matrix } from './components/matrix'
+
 import './App.css'
+import { useState } from 'react'
+import Matrix from './components/matrix'
+import Nav from './components/navigation';
+import Description from './components/description';
 
 function App() {
+  const [currentPage, setCurrentPage] = useState("description");
+
+  function changePage(text) {
+    setCurrentPage(text)
+  }
+
+  //  add conditional rendering for matrix / description(theory)
+
+  if (currentPage === "matrix") {
+    return (
+      <>
+        <header>
+          <Nav currentPage={currentPage} changePage={changePage} />
+        </header>
+        <main id="main">
+          <h1>Act matrix</h1>
+          <Matrix />
+        </main>
+      </>
+    )
+  }
+
+  else {
+    return (
+      <>
+        <header>
+          <Nav currentPage={currentPage} changePage={changePage} />
+        </header>
+        <main id="main">
+
+          <h1>Description / instructions / example</h1>
+          <Description />
+        </main>
+      </>
+    )
+  }
 
 
-  return (
-    <>
-      <main id="main">
-        <h1>Act matrix</h1>
-        <Matrix />
-      </main>
-    </>
-  )
 }
 
 export default App
