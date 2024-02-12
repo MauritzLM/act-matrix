83f93fb8a5916cf5089f4a2548066040f3b9ef0f
Mauritz Marais <mauritz303@gmail.com>
2024-02-12 13:06:29 +0200

    added component to display description text

---
+++ src/components/description.jsx
@@ -0,0 +1,36 @@
+
+export default function Description() {
+    return (
+        <div className="description-text">
+            <h2>Introduction</h2>
+            <p>The Matrix is a tool designed to help increase awareness, promote acceptance, and guide actions aligned with personal values</p>
+
+            <dl>
+                <dt>Exploration of Here and Now</dt>
+                <dd>Start by exploring the Here and Now quadrant. Become mindful of your present moment experiences, including thoughts, feelings, bodily sensations, and environmental cues.
+                    Emphasize nonjudgmental observation and acceptance of whatever arises.</dd>
+
+                <dt>Identification of Avoidance Behaviors</dt>
+                <dd>Move on to the Avoidance quadrant. Identify behaviors you engage in to avoid or suppress uncomfortable thoughts, emotions, or sensations.
+                    Examples may include distraction, substance use, or procrastination.</dd>
+
+                <dt>Recognition of Cognitive Fusion</dt>
+                <dd>Proceed to the Fusion quadrant. Recognize instances of cognitive fusion, where you become entangled with your thoughts and treat them as literal truths.
+                    Observe your thoughts from a perspective of detachment and non-attachment.</dd>
+
+                <dt>Clarification of Values</dt>
+                <dd>Finally, explore the Toward Values quadrant. Clarify personal values and identify actions aligned with those values.
+                    Consider what is truly important to you and how you can live in accordance with your values, even in the presence of difficult thoughts or emotions.</dd>
+
+                <dt>Integration and Practice</dt>
+                <dd>Integrate the insights gained from the Matrix into your daily life.
+                    Regular practice of mindfulness, acceptance, and values-based actions.
+                    How will you navigate challenges and opportunities for growth.</dd>
+
+                <dt>Reflection and Adjustment</dt>
+                <dd>Periodically revisit the Matrix to reflect on your progress and make adjustments as needed.
+                    Notice any changes in your awareness, acceptance, and alignment with your values, and to make course corrections as necessary</dd>
+            </dl>
+        </div>
+    )
+}
