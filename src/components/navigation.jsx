f6125f53695e2662364b080e589236bb1989eb53
Mauritz Marais <mauritz303@gmail.com>
2024-03-20 15:56:47 +0200

    updated imports and props

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,18 +1,23 @@
 import PropTypes from 'prop-types'
 
-export default function Nav({ changePage, currentPage }) {
-    // add on click toggle
+export default function Nav({ location }) {
+    
+    // render different nav for dashboard*
+
     return (
         <>
             <nav id="nav">
-                <button onClick={() => changePage("description")} className={(currentPage === 'description' ? 'cs-active' : '')}>description</button>
-                <button onClick={() => changePage("matrix")} className={(currentPage === 'matrix' ? 'cs-active' : '')}>matrix</button>
+                <ul>
+                    <li><a className={location === '/' ? 'cs-active' : ''} href='/'>Home</a></li>
+                    <li><a className={location === '/signup' ? 'cs-active' : ''} href='/signup'>Signup</a></li>
+                    <li><a className={location === '/login' ? 'cs-active' : ''} href='/login'>Login</a></li>
+                    <li><a className={location === '/act-matrix' ? 'cs-active' : ''} href='/act-matrix'>Matrix</a></li>
+                </ul>
             </nav>
         </>
     )
 }
 
 Nav.propTypes = {
-    changePage: PropTypes.func,
-    currentPage: PropTypes.string
+    location: PropTypes.string,
 };
