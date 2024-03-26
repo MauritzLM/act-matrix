9195d70119e5453364b20a5a52c1f56b53dc7a2b
Mauritz Marais <mauritz303@gmail.com>
2024-03-26 16:22:15 +0200

    implemented protected api call

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import './index.scss'
 
 const domain = import.meta.env.VITE_AUTH0_DOMAIN;
 const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+const audience = import.meta.env.VITE_AUTH0_API_AUDIENCE;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -15,7 +16,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         domain={domain}
         clientId={clientId}
         authorizationParams={{
-          redirect_uri: window.location.origin
+          redirect_uri: window.location.origin,
+          audience: audience,
+          scope: "read:current_user update:current_user_metadata"
         }}>
         <App />
       </Auth0Provider>
--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,8 +1,49 @@
 
 import { useAuth0 } from "@auth0/auth0-react";
+import { useState, useEffect } from "react";
 
 const Dashboard = () => {
-  const { user, isAuthenticated, isLoading } = useAuth0();
+  const { user, isAuthenticated, isLoading, getAccessTokenSilently } = useAuth0();
+
+  const [userMetadata, setUserMetadata] = useState(null);
+
+  // get user data
+  useEffect(() => {
+    const getUserMetadata = async () => {
+      const domain = import.meta.env.VITE_AUTH0_API_AUDIENCE;
+
+      try {
+        // get access token
+        const accessToken = await getAccessTokenSilently({
+          authorizationParams: {
+            audience: domain,
+            scope: "read:current_user",
+          },
+        });
+
+        // url
+        const userDetailsByIdUrl = `http://localhost:3000/matrix`;
+
+        const metadataResponse = await fetch(userDetailsByIdUrl, {
+          method: 'POST',
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ 'id': '6' })
+        });
+
+        const response = await metadataResponse.json();
+        console.log(response);
+
+        setUserMetadata(response);
+      } catch (e) {
+        console.log(e.message);
+      }
+    };
+
+    getUserMetadata();
+  }, [getAccessTokenSilently]);
 
   if (isLoading) {
     return <div>Loading ...</div>;
@@ -12,8 +53,11 @@ const Dashboard = () => {
     isAuthenticated && (
       <div>
         <img src={user.picture} alt={user.name} />
-        <h2>{user.name}</h2>
-        <p>{user.email}</p>
+        <h2>{user.nickname}</h2>
+        <p>{user.sub.split('auth0|')}</p>
+        <h3>User Metadata</h3>
+        <p>instance_id: {userMetadata.instance_id}</p>
+        <p>quadrant_1 content: {userMetadata.quadrant_1}</p>
       </div>
     )
   );
