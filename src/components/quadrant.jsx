76e1afdc7919fa7e5389efae125b01d5f309aa88
Mauritz Marais <mauritz303@gmail.com>
2024-03-28 16:48:54 +0200

    removed switch statement, replaced with usecontext

--- src/components/quadrant.jsx
+++ src/components/quadrant.jsx
@@ -1,34 +1,19 @@
 // quadrant component
 import PropTypes from 'prop-types'
 import TextEditor from './editor';
+import { useContext, useState } from 'react';
+import { userContext } from '../context/usercontext';
 
 export default function Quadrant({ title, id }) {
-    let placeholder;
-
-    switch (id) {
-        case 1:
-            placeholder = "How am I responding/reacting to my inner experience?";
-            break;
-        case 2:
-            placeholder = "What can I do to move toward what matters?";
-            break;
-
-        case 3:
-            placeholder = "What inner experience is showing up for me?";
-            break;
-
-        case 4:
-            placeholder = "What matters to me?";
-            break;
-    }
+    // const [placeholder, setPlaceholder] = useState('');
+    const userInfo = useContext(userContext);
 
     return (
         <>
             <div className="quadrant">
                 <label>{title}</label>
-                {/* <textarea id="quadrant-1" name="quadrant-1"></textarea>  */}
-                <div className="container">
-                    <TextEditor id={id} placeholder={placeholder} />
+                <div className="quadrant-container">
+                    <TextEditor id={id} content={userInfo.selectedMatrix[`quadrant_${id}`]} />
                 </div>
 
             </div>
