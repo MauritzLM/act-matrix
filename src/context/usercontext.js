8987b1bed94fe6ff36ed40b4a83a0468b625d89b
Mauritz Marais <mauritz303@gmail.com>
2024-03-28 16:50:30 +0200

    added initial value

--- src/context/usercontext.js
+++ src/context/usercontext.js
@@ -1,3 +1,3 @@
 import { createContext } from "react";
 
-export const userContext = createContext();
+export const userContext = createContext({ userMatrices: [], user: {}, selectedMatrix: {}, changeMatrix: () => { return 1 } });
