1691af0c1544ca8af768271e79289882a4d21639
Mauritz Marais <mauritz303@gmail.com>
2024-03-30 13:18:22 +0200

    added editor content state

--- src/components/quadrant.jsx
+++ src/components/quadrant.jsx
@@ -5,15 +5,15 @@ import { useContext, useState } from 'react';
 import { userContext } from '../context/usercontext';
 
 export default function Quadrant({ title, id }) {
-    // const [placeholder, setPlaceholder] = useState('');
     const userInfo = useContext(userContext);
-
+    const [editorContent, setEditorContent] = useState(userInfo.selectedMatrix[`quadrant_${id}`]);
+   
     return (
         <>
             <div className="quadrant">
                 <label>{title}</label>
                 <div className="quadrant-container">
-                    <TextEditor id={id} content={userInfo.selectedMatrix[`quadrant_${id}`]} />
+                    <TextEditor id={id} editorContent={editorContent} setEditorContent={setEditorContent} />
                 </div>
 
             </div>
@@ -24,8 +24,4 @@ export default function Quadrant({ title, id }) {
 Quadrant.propTypes = {
     title: PropTypes.string,
     id: PropTypes.number
-};
-
-Quadrant.defaultProps = {
-    title: 'Quadrant',
 };
