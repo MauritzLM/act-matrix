ce71a8aa14d4f3114b065d09c28f2e3206150499
Mauritz Marais <mauritz303@gmail.com>
2024-04-10 12:28:03 +0200

    added delete warning message and close svg

--- src/components/updateForm.jsx
+++ src/components/updateForm.jsx
@@ -4,13 +4,24 @@ export default function UpdateForm({ updateObj, setUpdate, updateFunction }) {
 
     return (
         <div id="update-form">
-            <button data-testid="close" onClick={() => setUpdate({ ...updateObj, type: '', errorMsg: '' })}>Close</button>
+
+            <button data-testid="close" className='close-btn' onClick={() => setUpdate({ ...updateObj, type: '', errorMsg: '' })}>
+                <img alt='' src='./src/assets/svgs/close.svg'></img>
+            </button>
+
             <form data-testid="form" onSubmit={(e) => updateFunction(e)}>
                 <label htmlFor="title">{updateObj.type}</label>
                 <input type="text" name="title" id="title"></input>
                 <span data-testid="error">{updateObj.errorMsg}</span>
+                
+                {/* delete warning */}
+                {updateObj.type === 'delete' && (
+                    <p data-testid="delete">Are you sure you want to delete {updateObj.title}?</p>
+                )}
+                
                 <button data-testid="submit">submit</button>
             </form>
+
         </div>
     )
 }
