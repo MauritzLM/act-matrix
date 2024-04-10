bc22b2840b26185a608b7385dde5488956e76953
Mauritz Marais <mauritz303@gmail.com>
2024-04-10 12:28:46 +0200

    added test for delete message

--- src/components/tests/updateForm.test.jsx
+++ src/components/tests/updateForm.test.jsx
@@ -4,18 +4,19 @@ import userEvent from "@testing-library/user-event";
 import UpdateForm from '../updateForm';
 
 describe('update form', () => {
-    const updateObj = { type: '', errorMsg: 'please enter a valid name', }
+    const updateObj = { type: 'profile', errorMsg: 'please enter a valid name', }
     const setUpdate = vi.fn()
     const updateFunction = vi.fn()
 
-    it('update form rendering', () => {
+    it('update form rendering with error message', () => {
 
         render(<UpdateForm updateObj={updateObj} setUpdate={setUpdate} updateFunction={updateFunction} />)
 
         expect(screen.getByTestId('form')).toBeInTheDocument()
         expect(screen.getByTestId('error')).toHaveTextContent('please enter a valid name')
+        expect(screen.queryByTestId('delete')).not.toBeInTheDocument()
 
-    })
+    });
 
     it('submit button testing', async () => {
         const user = userEvent.setup()
@@ -26,7 +27,7 @@ describe('update form', () => {
         await user.click(submitBtn)
 
         expect(updateFunction).toHaveBeenCalled()
-    })
+    });
 
     it('close button testing', async () => {
         const user = userEvent.setup()
@@ -37,6 +38,15 @@ describe('update form', () => {
         await user.click(closeBtn)
 
         expect(setUpdate).toHaveBeenCalled()
-    })
+    });
 
-})
+    it('delete message and no error', () => {
+        const deleteObj = {type: 'delete', title: 'One', errorMsg: ''}
+
+        render(<UpdateForm updateObj={deleteObj} setUpdate={setUpdate} updateFunction={updateFunction}/>)
+
+        expect(screen.getByText('Are you sure you want to delete One?')).toBeInTheDocument()
+        expect(screen.getByTestId('error')).toHaveTextContent('')
+    });
+
+});
