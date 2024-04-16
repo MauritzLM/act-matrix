b97fc5ad32c839c78a01fcfce9141a31f98efbe5
Mauritz Marais <mauritz303@gmail.com>
2024-04-16 15:57:49 +0200

    added mobile toggle

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -3,33 +3,59 @@ import { useAuth0 } from '@auth0/auth0-react';
 import { Link } from 'react-router-dom';
 import LoginButton from './login';
 import LogoutButton from './logout';
+import { useEffect, useState } from 'react';
 
 
 export default function Nav({ location, theme, switchTheme }) {
     const { isAuthenticated } = useAuth0();
+    const [active, setActive] = useState('');
+
+    function toggleActive() {
+        setActive(active === '' ? 'cs-active' : '')
+    }
+
+    // remove active class on mobile menu when navigating to new page
+    useEffect(() => {
+        setActive('');
+    }, [location])
 
     return (
         <>
-            <nav id="nav">
-                <ul>
-                    <li><Link className={location === '/' ? 'cs-active' : ''} to='/'>Home</Link></li>
-                    <li><Link className={location === '/act-matrix' ? 'cs-active' : ''} to='/act-matrix'>Matrix</Link></li>
-                    {isAuthenticated && (<li><Link className={location === '/dashboard' ? 'cs-active' : ''} to='/dashboard'>Dashboard</Link></li>)}
-                </ul>
+            <nav id="nav" className={active}>
 
-                {/* theme toggle */}
-                <button onClick={switchTheme} className='theme-toggle'>
-                    <img src={theme === 'light' ? './src/assets/svgs/dark_mode.svg' : './src/assets/svgs/light_mode.svg'}></img>
+                {/* <!--Mobile Nav Toggle--> */}
+                <button onClick={toggleActive} className={`cs-toggle ${active}`} aria-label="mobile menu toggle">
+                    <div className="cs-box" aria-hidden="true">
+                        <span className="cs-line cs-line1" aria-hidden="true"></span>
+                        <span className="cs-line cs-line2" aria-hidden="true"></span>
+                        <span className="cs-line cs-line3" aria-hidden="true"></span>
+                    </div>
                 </button>
 
-                {!isAuthenticated && (
-                    <LoginButton />
-                )}
+                <div className={`ul-wrapper ${active}`}>
+                    <ul>
+                        <li><Link className={location === '/' ? 'cs-active' : ''} to='/'>Home</Link></li>
+                        <li><Link className={location === '/act-matrix' ? 'cs-active' : ''} to='/act-matrix'>Matrix</Link></li>
+                        {isAuthenticated && (<li><Link className={location === '/dashboard' ? 'cs-active' : ''} to='/dashboard'>Dashboard</Link></li>)}
+                    </ul>
+                </div>
 
-                {isAuthenticated && (
 
-                    <LogoutButton />
-                )}
+                <div className='theme-login-wrapper'>
+                    {/* theme toggle */}
+                    <button onClick={switchTheme} className='theme-toggle'>
+                        <img src={theme === 'light' ? './src/assets/svgs/dark_mode.svg' : './src/assets/svgs/light_mode.svg'}></img>
+                    </button>
+
+                    {!isAuthenticated && (
+                        <LoginButton />
+                    )}
+
+                    {isAuthenticated && (
+
+                        <LogoutButton />
+                    )}
+                </div>
             </nav>
         </>
     )
