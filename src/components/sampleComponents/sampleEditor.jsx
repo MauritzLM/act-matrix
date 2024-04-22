c1e29973e05075e0c6f4328f0dd6594f91f6b970
Mauritz Marais <mauritz303@gmail.com>
2024-04-22 15:47:00 +0200

    added aria hidden true to editor button svgs

--- src/components/sampleComponents/sampleEditor.jsx
+++ src/components/sampleComponents/sampleEditor.jsx
@@ -30,7 +30,7 @@ const MenuBar = () => {
             }
             className={editor.isActive('bold') ? 'is-active' : ''}
           >
-            <img alt='bold' src='./src/assets/svgs/format_bold.svg'></img>
+            <img alt='bold' src='./src/assets/svgs/format_bold.svg' aria-hidden='true'></img>
           </button>
           <button title='italic'
             onClick={() => editor.chain().focus().toggleItalic().run()}
@@ -43,7 +43,7 @@ const MenuBar = () => {
             }
             className={editor.isActive('italic') ? 'is-active' : ''}
           >
-            <img alt='italic' src='./src/assets/svgs/format_italic.svg'></img>
+            <img alt='italic' src='./src/assets/svgs/format_italic.svg' aria-hidden='true'></img>
           </button>
           <button title='strikethrough'
             onClick={() => editor.chain().focus().toggleStrike().run()}
@@ -56,7 +56,7 @@ const MenuBar = () => {
             }
             className={editor.isActive('strike') ? 'is-active' : ''}
           >
-            <img alt='strikethrough' src='./src/assets/svgs/format_strikethrough.svg'></img>
+            <img alt='strikethrough' src='./src/assets/svgs/format_strikethrough.svg' aria-hidden='true'></img>
           </button>
           <button title='paragraph'
             onClick={() => editor.chain().focus().toggleCode().run()}
@@ -69,7 +69,7 @@ const MenuBar = () => {
             }
             className={editor.isActive('code') ? 'is-active' : ''}
           >
-            <img alt='paragraph' src='./src/assets/svgs/format_paragraph.svg'></img>
+            <img alt='paragraph' src='./src/assets/svgs/format_paragraph.svg' aria-hidden='true'></img>
           </button>
           {/* code
       <button onClick={() => editor.chain().focus().unsetAllMarks().run()}>
@@ -89,13 +89,13 @@ const MenuBar = () => {
             onClick={() => editor.chain().focus().toggleBulletList().run()}
             className={editor.isActive('bulletList') ? 'is-active' : ''}
           >
-            <img alt='bullet list' src='./src/assets/svgs/format_list_bulleted.svg'></img>
+            <img alt='bullet list' src='./src/assets/svgs/format_list_bulleted.svg' aria-hidden='true'></img>
           </button>
           <button title='ordered list'
             onClick={() => editor.chain().focus().toggleOrderedList().run()}
             className={editor.isActive('orderedList') ? 'is-active' : ''}
           >
-            <img alt='ordered list' src='./src/assets/svgs/format_list_numbered.svg'></img>
+            <img alt='ordered list' src='./src/assets/svgs/format_list_numbered.svg' aria-hidden='true'></img>
           </button>
           {/* <button
             onClick={() => editor.chain().focus().toggleCodeBlock().run()}
@@ -107,7 +107,7 @@ const MenuBar = () => {
             onClick={() => editor.chain().focus().toggleBlockquote().run()}
             className={editor.isActive('blockquote') ? 'is-active' : ''}
           >
-            <img alt='blockqoute' src='./src/assets/svgs/format_quote.svg'></img>
+            <img alt='blockqoute' src='./src/assets/svgs/format_quote.svg' aria-hidden='true'></img>
           </button>
         </div>
 
@@ -116,37 +116,37 @@ const MenuBar = () => {
             onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
             className={editor.isActive('heading', { level: 1 }) ? 'is-active' : ''}
           >
-            <img alt='heading 1' src='./src/assets/svgs/format_h1.svg'></img>
+            <img alt='heading 1' src='./src/assets/svgs/format_h1.svg' aria-hidden='true'></img>
           </button>
           <button title='heading 2'
             onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
             className={editor.isActive('heading', { level: 2 }) ? 'is-active' : ''}
           >
-            <img alt='heading 2' src='./src/assets/svgs/format_h2.svg'></img>
+            <img alt='heading 2' src='./src/assets/svgs/format_h2.svg' aria-hidden='true'></img>
           </button>
           <button title='heading 3'
             onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
             className={editor.isActive('heading', { level: 3 }) ? 'is-active' : ''}
           >
-            <img alt='heading 3' src='./src/assets/svgs/format_h3.svg'></img>
+            <img alt='heading 3' src='./src/assets/svgs/format_h3.svg' aria-hidden='true'></img>
           </button>
           <button title='heading 4'
             onClick={() => editor.chain().focus().toggleHeading({ level: 4 }).run()}
             className={editor.isActive('heading', { level: 4 }) ? 'is-active' : ''}
           >
-            <img alt='heading 4' src='./src/assets/svgs/format_h4.svg'></img>
+            <img alt='heading 4' src='./src/assets/svgs/format_h4.svg' aria-hidden='true'></img>
           </button>
           <button title='heading 5'
             onClick={() => editor.chain().focus().toggleHeading({ level: 5 }).run()}
             className={editor.isActive('heading', { level: 5 }) ? 'is-active' : ''}
           >
-            <img alt='heading 5' src='./src/assets/svgs/format_h5.svg'></img>
+            <img alt='heading 5' src='./src/assets/svgs/format_h5.svg' aria-hidden='true'></img>
           </button>
           <button title='heading 6'
             onClick={() => editor.chain().focus().toggleHeading({ level: 6 }).run()}
             className={editor.isActive('heading', { level: 6 }) ? 'is-active' : ''}
           >
-            <img alt='heading 6' src='./src/assets/svgs/format_h6.svg'></img>
+            <img alt='heading 6' src='./src/assets/svgs/format_h6.svg' aria-hidden='true'></img>
           </button>
 
         </div>
@@ -182,11 +182,11 @@ const MenuBar = () => {
         <div className='button-group'>
           <button title='horizontal rule'
             onClick={() => editor.chain().focus().setHorizontalRule().run()}>
-            <img alt='horizontal rule' src='./src/assets/svgs/horizontal_rule.svg'></img>
+            <img alt='horizontal rule' src='./src/assets/svgs/horizontal_rule.svg' aria-hidden='true'></img>
           </button>
           <button title='hard break'
             onClick={() => editor.chain().focus().setHardBreak().run()}>
-            <img alt='hard break' src='./src/assets/svgs/keyboard_return.svg'></img>
+            <img alt='hard break' src='./src/assets/svgs/keyboard_return.svg' aria-hidden='true'></img>
           </button>
           <button title='undo'
             onClick={() => editor.chain().focus().undo().run()}
@@ -198,7 +198,7 @@ const MenuBar = () => {
                 .run()
             }
           >
-            <img alt='undo' src='./src/assets/svgs/undo.svg'></img>
+            <img alt='undo' src='./src/assets/svgs/undo.svg' aria-hidden='true'></img>
           </button>
           <button title='redo'
             onClick={() => editor.chain().focus().redo().run()}
@@ -210,7 +210,7 @@ const MenuBar = () => {
                 .run()
             }
           >
-            <img alt='redo' src='./src/assets/svgs/redo.svg'></img>
+            <img alt='redo' src='./src/assets/svgs/redo.svg' aria-hidden='true'></img>
           </button>
 
           <button
