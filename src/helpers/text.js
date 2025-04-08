025ea03a529e421382747b5352aec40f738b9c5e
Mauritz Marais <mauritz303@gmail.com>
2025-04-08 11:42:59 +0200

    removed comments

--- src/helpers/text.js
+++ src/helpers/text.js
@@ -1,24 +1,19 @@
 import { compile } from "html-to-text";
 
-// set options for headings -> not uppercase
-
-// create table for content?
-
 // options
 const options = {
     selectors: [
         { selector: 'h2', format: 'heading', options: { leadingLineBreaks: 2, trailingLineBreaks: 2, uppercase: false } },
         { selector: 'h3', format: 'heading', options: { leadingLineBreaks: 2, trailingLineBreaks: 2, uppercase: false } },
         { selector: 'h4', format: 'heading', options: { leadingLineBreaks: 2, trailingLineBreaks: 2, uppercase: false } },
-    ]
+        // { selector: 'table', format: 'dataTable' }
+    ],
 }
 
 const compiledConvert = compile(options)
 
 export function convertHtml(arr) {
     const texts = arr.map(compiledConvert)
-
-    // save as text file*
 
     return texts.join('\n\n')
 }
